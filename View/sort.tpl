9ffa91a602e68531f4fb6495af1a34714d364955
Arlen Hills <arlen.hills@wsu.edu>
2024-02-17 14:03:01 -0800

    added framework for all needed routes

---
+++ View/sort.tpl
@@ -0,0 +1,8 @@
+<html> 
+	<head>
+		<title>TrueShuffle</title> 
+	</head> 
+	<body> 
+		<h1>choose your sort</h1> 
+	</body> 
+</html>
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from bottle import route, run, template 
+from bottle import route, run, template, get, post, request 
 
 #get route for home page, should be a page to login with spotify account
 @route('/', method='GET') 
@@ -12,10 +12,16 @@ def doLogin():
 	#handle spotify login logic, redirect to get method on fail, redirect to playlist select on succeed
 	return template('View/index.tpl') 
 
-#
+#get route for selecting playlist
 @route('/select', method='GET')
 def select():
+	#should list all user's saved playlists and let them select one
+	return template('View/select.tpl')
 
+#post route for selecting playlist
+@route('/select', method='GET')
+def select():
+	#should send user to sort page where they pick the sort option and override playlist
 	return template('View/select.tpl')
 
 run(host='localhost', port=8080,debug=True)
